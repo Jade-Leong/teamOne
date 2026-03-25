bbb8f1742ea7da9e67b0300b093a56fbaa7bc9ab
Jade Leong <jyl9618@nyu.edu>
2026-03-25 15:53:32 -0400

    added sprite pic

---
+++ script.js
@@ -0,0 +1,4 @@
+const supabaseUrl = "https://rkqcnibvsvbjxdwjklim.supabase.co";
+const supabaseAnonKey = "sb_publishable_08eKwdrsBXZWTzvH04hzJg_G6Z-ZgnV";
+
+const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
